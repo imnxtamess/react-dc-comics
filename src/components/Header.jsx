611c8e964f1ac1a1a2ebbfb0a4abcaabe0cc9b46
imnxtamess <davide.morisco.work@gmail.com>
2025-03-20 14:08:02 +0100

    add navigation menu to header and apply global styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,45 @@
 export default function Header() {
   // LOGIC HERE
 
-  return <header>TEST</header>;
+  return (
+    <header>
+      <nav className="menu">
+        <div className="logo">
+          <img src="/dc-logo.png" alt="" />
+        </div>
+        <ul className="unstyled-list">
+          <li>
+            <a href="">characters</a>
+          </li>
+          <li>
+            <a href="">comics</a>
+          </li>
+          <li>
+            <a href="">movies</a>
+          </li>
+          <li>
+            <a href="">tv</a>
+          </li>
+          <li>
+            <a href="">games</a>
+          </li>
+          <li>
+            <a href="">collectibles</a>
+          </li>
+          <li>
+            <a href="">videos</a>
+          </li>
+          <li>
+            <a href="">fans</a>
+          </li>
+          <li>
+            <a href="">news</a>
+          </li>
+          <li>
+            <a href="">shop</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
