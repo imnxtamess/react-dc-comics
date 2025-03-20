ea004e28765531522a3b1c53e75bf2e22b3effec
imnxtamess <davide.morisco.work@gmail.com>
2025-03-20 14:33:54 +0100

    add Main and Features components with styling for content section

---
+++ src/components/Features.jsx
@@ -0,0 +1,5 @@
+export default function Features() {
+  // LOGIC HERE
+
+  return <section className="features">FEATURES</section>;
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,13 @@
+import Features from "./Features";
+
 export default function Main() {
   // LOGIC HERE
 
-  return <main>TEST</main>;
+  return (
+    <main>
+      <div className="container">
+        <h1>--&gt; Content goes here &lt;--</h1>
+      </div>
+    </main>
+  );
 }
