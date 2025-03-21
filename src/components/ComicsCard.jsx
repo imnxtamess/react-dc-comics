d13ef6f6ca13a6bdb4041048a90dfae8829c9acf
imnxtamess <davide.morisco.work@gmail.com>
2025-03-21 16:27:59 +0100

    refactor ComicsCard and ComicsList components; integrate comics data for dynamic rendering and update image sources

--- src/components/ComicsCard.jsx
+++ src/components/ComicsCard.jsx
@@ -2,11 +2,13 @@ export default function ComicsCard({ img, title }) {
   // Logic here
 
   return (
-    <div className="card">
-      <div className="card_header">
-        <img src={img} alt={title} />
+    <div className="col-2">
+      <div className="card">
+        <div className="card_header">
+          <img src={img} alt={title} />
+        </div>
+        <span>{title}</span>
       </div>
-      <span>{title}</span>
     </div>
   );
 }
