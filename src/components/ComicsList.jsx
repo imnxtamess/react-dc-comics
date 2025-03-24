dde044db88101eda0c6015233729ebfc33e67455
imnxtamess <davide.morisco.work@gmail.com>
2025-03-24 18:49:32 +0100

    add filtering functionality to ComicsList component with multiple criteria

--- src/components/ComicsList.jsx
+++ src/components/ComicsList.jsx
@@ -1,15 +1,24 @@
 import ComicsCard from "./ComicsCard";
 import comics from "../data/comics";
-import React, { useState } from "react";
+import { useState } from "react";
 export default function ComicsList() {
-  const [searchQuery, setSearchQuery] = useState(""); // State to store the search query
+  const [searchQuery, setSearchQuery] = useState(""); // State to store the Search query
+  const [typeQuery, setTypeQuery] = useState(""); // State to store the Type query
+  const [seriesQuery, setSeriesQuery] = useState(""); // State to store the Series query
+  const [artistsQuery, setArtistsQuery] = useState(""); // State to store the Artists query
 
-  // Filter comics based on the search query
+  // Filter comics based on the queries
   const filteredComics = comics.filter((comic) => {
-    return comic.title.toLowerCase().includes(searchQuery.toLowerCase());
+    return (
+      comic.title.toLowerCase().includes(searchQuery.toLowerCase()) &&
+      comic.type.toLowerCase().includes(typeQuery.toLowerCase()) &&
+      comic.series.toLowerCase().includes(seriesQuery.toLowerCase()) &&
+      (comic.artists.some((artist) =>
+        artist.toLowerCase().includes(artistsQuery.toLowerCase())
+      ) ||
+        artistsQuery === "")
+    );
   });
-
-  console.log(filteredComics);
 
   return (
     <div className="comicsContainer">
@@ -17,10 +26,52 @@ export default function ComicsList() {
       <div className="searchBar">
         <input
           type="text"
-          placeholder="Search..."
+          placeholder="Filter by Title...🔍"
           value={searchQuery} // Bind input value to state
           onChange={(e) => setSearchQuery(e.target.value)} // Update state on input change
         />
+      </div>
+      <div className="filtersContainer">
+        <div className="typeFilter">
+          <span>TYPE</span>
+          <select
+            name="filterType"
+            value={typeQuery} // Bind input value to state
+            onChange={(e) => setTypeQuery(e.target.value)} // Update sate on input change
+          >
+            <option value="">Select a Type</option>
+            <option value="comic book">COMIC BOOK</option>
+            <option value="graphic novel">GRAPHIC NOVEL</option>
+          </select>
+        </div>
+        <div className="seriesFilter">
+          <span>SERIES</span>
+          <select
+            name="filterSeries"
+            onChange={(e) => setSeriesQuery(e.target.value)}
+            value={seriesQuery}
+          >
+            <option value="">Select a Series</option>
+            {comics.map((comic) => (
+              <option value={comic.series}>{comic.series}</option>
+            ))}
+          </select>
+        </div>
+        <div className="creatorsFilter">
+          <span>CREATORS</span>
+          <select
+            name="filterArtists"
+            onChange={(e) => setArtistsQuery(e.target.value)}
+            value={artistsQuery}
+          >
+            <option value="">Select an Artist</option>
+            {[...new Set(comics.flatMap((comic) => comic.artists))].map(
+              (artist) => {
+                return <option value={artist}>{artist}</option>;
+              }
+            )}
+          </select>
+        </div>
       </div>
       <div className="row">
         {filteredComics.map((comic) => (
