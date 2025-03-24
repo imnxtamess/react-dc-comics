915718b45bf96f4a91f8122e63ae212c167ad309
imnxtamess <davide.morisco.work@gmail.com>
2025-03-24 16:10:18 +0100

    add search functionality to ComicsList component and style search bar

--- src/components/ComicsList.jsx
+++ src/components/ComicsList.jsx
@@ -1,13 +1,29 @@
 import ComicsCard from "./ComicsCard";
 import comics from "../data/comics";
+import React, { useState } from "react";
 export default function ComicsList() {
-  //Logic here
-  console.log(comics);
+  const [searchQuery, setSearchQuery] = useState(""); // State to store the search query
+
+  // Filter comics based on the search query
+  const filteredComics = comics.filter((comic) => {
+    return comic.title.toLowerCase().includes(searchQuery.toLowerCase());
+  });
+
+  console.log(filteredComics);
+
   return (
     <div className="comicsContainer">
       <div className="badge">CURRENT SERIES</div>
+      <div className="searchBar">
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery} // Bind input value to state
+          onChange={(e) => setSearchQuery(e.target.value)} // Update state on input change
+        />
+      </div>
       <div className="row">
-        {comics.map((comic) => (
+        {filteredComics.map((comic) => (
           <ComicsCard key={comic.id} img={comic.thumb} title={comic.title} />
         ))}
       </div>
